42c0e304bbeb16990a24010a757af24adbc0c48e
mayur <mayurbhosalen9@example.com>
2025-05-02 16:53:10 +0530

    changed region

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-terraform-state-bucket"
+    bucket = "jenkins-terraform-state-bucket-project1"
     region = "us-east-1"
     key = "eks/terraform.tfstate"
   }
